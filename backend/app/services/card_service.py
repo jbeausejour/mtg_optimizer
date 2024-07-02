464c957f506843bb32d32808f4c5834b74a3a8cf
jbeausejour <jules.beausejour@gmail.com>
2024-07-02 16:51:17 -0400

    adding the optimization back end

--- backend/app/services/card_service.py
+++ backend/app/services/card_service.py
@@ -1,109 +1,156 @@
+# card_service.py
+
 import requests
 import urllib
 from htmldom import htmldom
 from html import unescape
 from fuzzywuzzy import process
-from app.models.card import Card_list
+from app.models.card import Card, Card_list
+from app.utils.data_fetcher import DataFetcher
+from app.utils.optimization import OptimizationEngine
+import pandas as pd
+from app.utils.optimization import OptimizationEngine
 
 SCRYFALL_API_URL = "https://api.scryfall.com/cards/named"
 SCRYFALL_SEARCH_API_URL = "https://api.scryfall.com/cards/search"
 MTGSTOCKS_BASE_URL = 'http://www.mtgstocks.com'
 QUERY_STRING = '/cards/search?utf8=%E2%9C%93&print%5Bcard%5D={}&button='
 
-def get_all_cards():
-    print('Getting cards !')
-    return Card_list.query.all()
+class CardService:
+    @staticmethod
+    def get_all_cards():
+        print('Getting cards !')
+        return Card_list.query.all()
 
-def get_card_versions(card_name):
-    response = requests.get(SCRYFALL_SEARCH_API_URL, params={'q': f'!"{card_name}"'})
-    response.raise_for_status()
-    
-    data = response.json()
-    sets = set()
-    languages = set()
-    versions = set()
+    @staticmethod
+    def get_card_versions(card_name):
+        response = requests.get(SCRYFALL_SEARCH_API_URL, params={'q': f'!"{card_name}"'})
+        response.raise_for_status()
+        
+        data = response.json()
+        sets = set()
+        languages = set()
+        versions = set()
 
-    for card in data['data']:
-        sets.add((card['set'], card['set_name']))
-        languages.add(card['lang'])
-        if 'finishes' in card:
-            versions.update(card['finishes'])
+        for card in data['data']:
+            sets.add((card['set'], card['set_name']))
+            languages.add(card['lang'])
+            if 'finishes' in card:
+                versions.update(card['finishes'])
 
-    return {
-        'name': card_name,
-        'sets': [{'code': code, 'name': name} for code, name in sets],
-        'languages': list(languages),
-        'versions': list(versions)
-    }
+        return {
+            'name': card_name,
+            'sets': [{'code': code, 'name': name} for code, name in sets],
+            'languages': list(languages),
+            'versions': list(versions)
+        }
 
-def fetch_card_data(card_name, set_code=None, language=None, version=None):
-    scryfall_data = fetch_scryfall_data(card_name, set_code, language)
-    mtgstocks_data = fetch_mtgstocks_data(card_name, set_code)
+    @staticmethod
+    def fetch_card_data(card_name, set_code=None, language=None, version=None):
+        scryfall_data = CardService.fetch_scryfall_data(card_name, set_code, language)
+        mtgstocks_data = CardService.fetch_mtgstocks_data(card_name, set_code)
 
-    # Add version information to Scryfall data
-    scryfall_data['version'] = version
+        # Add version information to Scryfall data
+        scryfall_data['version'] = version
 
-    return {
-        'scryfall': scryfall_data,
-        'mtgstocks': mtgstocks_data,
-    }
+        return {
+            'scryfall': scryfall_data,
+            'mtgstocks': mtgstocks_data,
+        }
 
-def fetch_scryfall_data(card_name, set_code=None, language=None):
-    params = {'fuzzy': card_name}
-    if set_code:
-        params['set'] = set_code
-    if language:
-        params['lang'] = language
+    @staticmethod
+    def fetch_scryfall_data(card_name, set_code=None, language=None):
+        params = {'fuzzy': card_name}
+        if set_code:
+            params['set'] = set_code
+        if language:
+            params['lang'] = language
 
-    response = requests.get(SCRYFALL_API_URL, params=params)
-    response.raise_for_status()
-    return response.json()
+        response = requests.get(SCRYFALL_API_URL, params=params)
+        response.raise_for_status()
+        return response.json()
 
-def fetch_mtgstocks_data(card_name, set_code=None):
-    search_name = f"{card_name} ({set_code})" if set_code else card_name
-    card_url = card_url_from_name(search_name)
-    return scrape_price(card_url) if card_url else {'error': 'Card not found'}
+    @staticmethod
+    def fetch_mtgstocks_data(card_name, set_code=None):
+        search_name = f"{card_name} ({set_code})" if set_code else card_name
+        card_url = CardService.card_url_from_name(search_name)
+        return CardService.scrape_price(card_url) if card_url else {'error': 'Card not found'}
 
-def generate_search_url(name):
-    formatted_name = urllib.parse.quote('+'.join(name.split(' ')), '/+')
-    return MTGSTOCKS_BASE_URL + QUERY_STRING.format(formatted_name)
+    @staticmethod
+    def generate_search_url(name):
+        formatted_name = urllib.parse.quote('+'.join(name.split(' ')), '/+')
+        return MTGSTOCKS_BASE_URL + QUERY_STRING.format(formatted_name)
 
-def get_matching_item_on_page(url, text, selector):
-    page = htmldom.HtmlDom(url)
-    page.createDom()
-    elems = page.find(selector)
-    possible_matches = [elem.text() for elem in elems]
-    best_match = process.extractOne(text, possible_matches)
-    match_index = possible_matches.index(best_match[0])
-    return elems[match_index]
+    @staticmethod
+    def get_matching_item_on_page(url, text, selector):
+        page = htmldom.HtmlDom(url)
+        page.createDom()
+        elems = page.find(selector)
+        possible_matches = [elem.text() for elem in elems]
+        best_match = process.extractOne(text, possible_matches)
+        match_index = possible_matches.index(best_match[0])
+        return elems[match_index]
 
-def get_card_url_from_search_results(search_url, name):
-    card_link = get_matching_item_on_page(search_url, name, '.table > tbody > tr > td > a')
-    return MTGSTOCKS_BASE_URL + card_link.attr('href')
+    @staticmethod
+    def get_card_url_from_search_results(search_url, name):
+        card_link = CardService.get_matching_item_on_page(search_url, name, '.table > tbody > tr > td > a')
+        return MTGSTOCKS_BASE_URL + card_link.attr('href')
 
-def card_url_from_name(name):
-    query_url = generate_search_url(name)
-    response = requests.get(query_url, allow_redirects=False)
-    if response.status_code in range(301, 307):
-        return response.headers['Location']
-    elif response.status_code == requests.codes.ok:
-        return get_card_url_from_search_results(query_url, name)
-    return None
+    @staticmethod
+    def card_url_from_name(name):
+        query_url = CardService.generate_search_url(name)
+        response = requests.get(query_url, allow_redirects=False)
+        if response.status_code in range(301, 307):
+            return response.headers['Location']
+        elif response.status_code == requests.codes.ok:
+            return CardService.get_card_url_from_search_results(query_url, name)
+        return None
 
-def scrape_price(card_url):
-    card_page = htmldom.HtmlDom(card_url)
-    card_page.createDom()
-    card_name = card_page.find('h2 > a').text()
-    card_set = card_page.find('h5 > a').text()
-    price_values = [elem.text() for elem in card_page.find('.priceheader')]
-    price_keys = ['avg']
-    if len(price_values) > 1:
-        price_keys.insert(0, 'low')
-        price_keys.append('high')
-    return {
-        'name': unescape(card_name),
-        'set': unescape(card_set),
-        'link': card_url,
-        'promo': len(price_keys) == 1,
-        'prices': dict(zip(price_keys, price_values))
-    }
+    @staticmethod
+    def scrape_price(card_url):
+        card_page = htmldom.HtmlDom(card_url)
+        card_page.createDom()
+        card_name = card_page.find('h2 > a').text()
+        card_set = card_page.find('h5 > a').text()
+        price_values = [elem.text() for elem in card_page.find('.priceheader')]
+        price_keys = ['avg']
+        if len(price_values) > 1:
+            price_keys.insert(0, 'low')
+            price_keys.append('high')
+        return {
+            'name': unescape(card_name),
+            'set': unescape(card_set),
+            'link': card_url,
+            'promo': len(price_keys) == 1,
+            'prices': dict(zip(price_keys, price_values))
+        }
+
+    # New methods for optimization
+
+    @staticmethod
+    async def update_card_data():
+        await DataFetcher.update_all_cards()
+
+    @staticmethod
+    def optimize_card_purchases(strategy='milp', min_store=None, find_min_store=False):
+        card_details_df = pd.read_sql(Card.query.statement, Card.query.session.bind)
+        buylist_df = pd.read_sql(Card_list.query.statement, Card_list.query.session.bind)
+
+        optimization_engine = OptimizationEngine(card_details_df, buylist_df)
+
+        if strategy == 'milp':
+            return optimization_engine.run_milp_optimization(min_store, find_min_store)
+        elif strategy == 'nsga_ii':
+            return optimization_engine.run_nsga_ii_optimization()
+        elif strategy == 'hybrid':
+            milp_solution, _ = optimization_engine.run_milp_optimization(min_store, find_min_store)
+            return optimization_engine.run_nsga_ii_optimization(milp_solution)
+        else:
+            raise ValueError("Invalid optimization strategy")
+
+    @staticmethod
+    def get_purchasing_plan(solution):
+        card_details_df = pd.read_sql(Card.query.statement, Card.query.session.bind)
+        buylist_df = pd.read_sql(Card_list.query.statement, Card_list.query.session.bind)
+        optimization_engine = OptimizationEngine(card_details_df, buylist_df)
+        return optimization_engine.get_purchasing_plan(solution)
--- backend/app/utils/data_fetcher.py
+++ backend/app/utils/data_fetcher.py
@@ -1,18 +1,22 @@
-# data_fetcher.py
-
 import asyncio
 import aiohttp
 import logging
+import re
+import pandas as pd
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 
 from app.models.site import Site
 from app.models.card import Card, Card_list
 from app.models.scan import Scan, ScanResult
+from app.utils.helpers import clean_card_name, parse_card_string, extract_numbers
 from backend.app import db
 
-
 logger = logging.getLogger(__name__)
+
+STRATEGY_ADD_TO_CART = 1
+STRATEGY_SCRAPPER = 2
+STRATEGY_HAWK = 3
 
 class DataFetcher:
     def __init__(self):
@@ -76,11 +80,6 @@ class DataFetcher:
             self.save_cards_to_db(site, cards_df, card_names)
 
     @staticmethod
-    def extract_info(soup, site, card_names):
-        # We'll refactor this method separately
-        pass
-
-    @staticmethod
     def save_cards_to_db(site, cards_df, card_names):
         scan = Scan(card_names=card_names)
         db.session.add(scan)
@@ -120,3 +119,228 @@ class DataFetcher:
         async with cls() as fetcher:
             tasks = [fetcher.process_site(site, card_names) for site in sites]
             await asyncio.gather(*tasks)
+    
+    @staticmethod
+    def extract_info(soup, site, card_names, strategy):
+        if soup is None:
+            logger.warning(f"Soup is None for site {site}")
+            return pd.DataFrame()
+
+        cards = []
+        seen_variants = set()
+
+        excluded_categories = {
+            'playmats', 'booster packs', 'booster box', 'mtg booster boxes',
+            'art series', 'fat packs and bundles', 'mtg booster packs',
+            'magic commander deck', 'world championship deck singles',
+            'The Crimson Moon\'s Fairy Tale', 'rpg accessories', 'scan other',
+            'intro packs and planeswalker decks', 'wall scrolls'
+        }
+
+        if strategy == STRATEGY_HAWK:
+            return pd.DataFrame([card.to_dict() for card in DataFetcher.strategy_hawk(soup)])
+
+        content = soup.find('div', {'class': ['content', 'content clearfix', 'content inner clearfix']})
+        if content is None:
+            logger.error(f"Content div not found for site {site}")
+            return pd.DataFrame()
+
+        products_containers = content.find_all('div', {'class': 'products-container browse'})
+        if not products_containers:
+            logger.warning(f"No products container found for site {site}")
+            return pd.DataFrame()
+
+        for container in products_containers:
+            for item in container.find_all('li', {'class': 'product'}):
+                card = DataFetcher.process_product_item(item, site, card_names, excluded_categories)
+                if card:
+                    DataFetcher.process_variants(item, card, cards, seen_variants, strategy)
+
+        return pd.DataFrame([card.to_dict() for card in cards])
+
+    @staticmethod
+    def process_product_item(item, site, card_names, excluded_categories):
+        if DataFetcher.is_yugioh_card(item):
+            return None
+
+        meta = item.find('div', {'class': 'meta'})
+        test_category = meta.find('span', {'class': 'category'}).text.strip()
+        test_title = meta.find('h4', {'class': 'name'}).text.strip()
+
+        if any(cat in test_category.lower() for cat in excluded_categories):
+            return None
+
+        card = parse_card_string(test_title)
+        card.Site = site
+        card.Name = clean_card_name(card.Name, card_names)
+        card.Edition = test_category
+
+        if not card.Name or card.Name not in card_names and card.Name.split(' // ')[0].strip() not in card_names:
+            return None
+
+        return card
+
+    @staticmethod
+    def process_variants(item, card, cards, seen_variants, strategy):
+        variants = item.find('div', {'class': 'variants'})
+        for variant in variants.find_all('div', {'class': 'variant-row'}):
+            card_variant = DataFetcher.strategy_add_to_cart(card, variant) if strategy == STRATEGY_ADD_TO_CART else DataFetcher.strategy_scrapper(card, variant)
+            if card_variant is not None and card_variant not in seen_variants:
+                cards.append(card_variant)
+                seen_variants.add(card_variant)
+
+    @staticmethod
+    def is_yugioh_card(item):
+        image = item.find('div', {'class': 'image'})
+        a_tag = image and image.find('a', href=True)
+        return a_tag and 'yugioh' in a_tag['href']
+
+    @staticmethod
+    def strategy_add_to_cart(card, variant):
+        if 'no-stock' in variant.get('class', []) or '0 In Stock' in variant:
+            return None
+        
+        form_element = variant.find('form', {'class': 'add-to-cart-form'})
+        if not form_element:
+            return None
+
+        attributes = form_element.attrs
+        if 'data-name' not in attributes:
+            return None
+
+        unclean_name, product_version, product_foil = DataFetcher.find_name_version_foil(attributes['data-name'])
+
+        if not card.Foil:
+            card.Foil = product_foil
+        if not card.Edition:
+            card.Edition = product_version
+
+        quality_language = DataFetcher.normalize_variant_description(attributes['data-variant'])
+        quality, language = quality_language[:2]
+
+        select_tag = variant.find('select', {'class': 'qty'}) or variant.find('input', {'class': 'qty'})
+        qty_available = select_tag['max'] if select_tag and 'max' in select_tag.attrs else "0"
+
+        card.Quality = quality
+        card.Language = language
+        card.Quantity = int(qty_available)
+        card.Edition = attributes['data-category']
+        card.Price = DataFetcher.normalize_price(attributes['data-price'])
+
+        return card
+
+    @staticmethod
+    def strategy_scrapper(card, variant):
+        if 'no-stock' in variant.get('class', []) or '0 In Stock' in variant:
+            return None
+
+        try:
+            quality, language = DataFetcher.extract_quality_language(card, variant)
+            if quality is None or language is None:
+                return None
+
+            quantity = DataFetcher.extract_quantity(card, variant)
+            if quantity is None:
+                return None
+
+            price = DataFetcher.extract_price(card, variant)
+
+            card.Quality = quality
+            card.Language = language
+            card.Quantity = quantity
+            card.Price = price
+
+            return card
+        except Exception as e:
+            logger.exception(f"Error in strategy_scrapper for {card.Name}: {str(e)}")
+            return None
+
+    @staticmethod
+    def strategy_hawk(soup):
+        cards_data = []
+        for card_div in soup.select('.hawk-results__item'):
+            card_details = {
+                'name': card_div.select_one('.hawk-results__hawk-contentTitle').get_text(strip=True),
+                'image_url': card_div.select_one('.hawk-results__item-image img')['src'],
+                'edition': card_div.select_one('.hawk-results__hawk-contentSubtitle').get_text(strip=True),
+                'variants': [],
+                'stock': [],
+                'prices': []
+            }
+            
+            for variant_div in card_div.select('.hawk-results__hawk-contentVariants input[type="radio"]'):
+                variant_details = {
+                    'variant_id': variant_div['id'],
+                    'condition': variant_div.get('data-options', '').split(',')[0].split('|')[1] if 'condition' in variant_div.get('data-options', '') else '',
+                    'finish': variant_div.get('data-options', '').split(',')[1].split('|')[1] if 'finish' in variant_div.get('data-options', '') else ''
+                }
+                card_details['variants'].append(variant_details)
+            
+            for stock_span in card_div.select('.hawkStock'):
+                card_details['stock'].append({
+                    'variant_id': stock_span['data-var-id'],
+                    'in_stock': stock_span.get_text(strip=True)
+                })
+            for price_span in card_div.select('.hawkPrice'):
+                card_details['prices'].append({
+                    'variant_id': price_span['data-var-id'],
+                    'price': price_span.get_text(strip=True)
+                })
+            
+            cards_data.append(card_details)
+        
+        return cards_data
+
+    @staticmethod
+    def find_name_version_foil(place_holder):
+        items = re.split(r' - ', place_holder)
+        items = [x.strip() for x in items]
+        product_name = items[0]
+        product_version = ""
+        product_foil = ""
+
+        for item in items[1:]:
+            if "Foil" in item:
+                product_foil = item
+            else:
+                product_version = item
+
+        return product_name, product_version, product_foil
+
+    @staticmethod
+    def normalize_variant_description(variant_description):
+        cleaned_description = variant_description.split(':')[-1].strip()
+        variant_parts = cleaned_description.split(',')
+        return [part.strip() for part in variant_parts]
+
+    @staticmethod
+    def extract_quality_language(card, variant):
+        variant_description = variant.find('span', {'class': 'variant-short-info variant-description'}) or \
+                              variant.find('span', {'class': 'variant-short-info'})
+        if variant_description:
+            quality_language = DataFetcher.normalize_variant_description(variant_description.text)
+            return quality_language[:2]
+        else:
+            logger.error(f"Error in extract_quality_language for {card.Name}: variant-description not found")
+            return None, None
+
+    @staticmethod
+    def extract_quantity(card, variant):
+        variant_qty = variant.find('span', {'class': 'variant-short-info variant-qty'}) or \
+                      variant.find('span', {'class': 'variant-short-info'})
+        if variant_qty:
+            variant_qty = variant_qty.text.strip()
+            return extract_numbers(variant_qty)
+        else:
+            logger.error(f"Error in extract_quantity for {card.Name}: variant-qty not found")
+            return None
+
+    @staticmethod
+    def extract_price(card, variant):
+        price_elem = variant.find('span', {'class': 'regular price'})
+        if price_elem is not None:
+            price_text = price_elem.text
+            return DataFetcher.normalize_price(price_text)
+        else:
+            logger.error(f"Error in extract_price for {card.Name}: Price element not found")
+            return 0.0
